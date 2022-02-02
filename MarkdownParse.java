f0d8c2cf4e3fc52178eb9c5ad454fbfc35f50dfa
Maryamkusman <93306676+Maryamkusman@users.noreply.github.com>
2022-02-02 17:27:28 -0600

    changes

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,4 +1,3 @@
-
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,31 +7,49 @@ import java.util.ArrayList;
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
+
+        //Split the contents of the markdown file into each individual line
+        String[] contentsArray = markdown.split("\n");
+        //Loop through each line, check if it is of a link form
+        //if so, add the solely the link into the return ArrayList
+        for(String s: contentsArray){
+            if(isOfLinkForm(s))
+                toReturn.add(s.substring(s.indexOf("(")+1, s.lastIndexOf(")")));
+                //using lastIndexOf() to fix a whitespace issue (I think) on
+                //windows
+        }
+        /* Original Code
+        -----------------------
         // find the next [, then find the ], then find the (, then take up to
         // the next )
-        int links = 0;
-        for(int i = 0; i < markdown.length(); i++){
-            if(markdown.charAt(i) == ')'){
-                links++;
-            }
-        }
         int currentIndex = 0;
-        while(currentIndex < markdown.length() && links>0) {
+        while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(nextCloseBracket == openParen-1 && nextOpenBracket != nextCloseBracket-1){
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            links--;
         }
+        //*/
         return toReturn;
     }
+    public static boolean isOfLinkForm(String s){
+        int firstBracket = s.indexOf("[");
+        int secondBracket = s.indexOf("]");
+        boolean containerGood = false;
+        String linkContainer = "";
+        //this obtains the whole container like [Link]
+        if (firstBracket >= 0 && secondBracket >= 0 && secondBracket > firstBracket)
+            linkContainer = s.substring(firstBracket,secondBracket+1);
+        //link container would be valid if its length is greater than 2 (it's not just "[]")
+        if(linkContainer.length() > 2)
+            containerGood = true;
 
+        return containerGood && s.contains("(") && s.contains(")") && !s.startsWith("!");
+    }
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
+		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
